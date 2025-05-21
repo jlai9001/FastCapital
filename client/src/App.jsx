3e143510ac74eaa66229a2e4e671310b545a1f99
Jonathan Lai <jlai9001@gmail.com>
2025-05-21 11:37:58 -0700

    homepage-hero complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter,Routes,Route, Navigate} from "react-router-dom";
+import HomePageHero from "./pages/homepage_hero";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+             {/* default home page when starting development server */}
+            <Route path="/" element={<Navigate to="/homepage_hero" />} />
+
+            <Route path="/homepage_hero" element={<HomePageHero />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
 
 export default App
+
+
+
+// reference
+// Router Setup
+
+// import {BrowserRouter,Routes,Route} from "react-router-dom";
+// import Home from "./pages/home";
+// import FullCollection from "./pages/collection";
+// import GameDetails from "./pages/game_detail";
+// import AddGame from "./pages/add_game";
+
+// function App() {
+//   return (
+
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/home" element={<Home/>} />
+//         <Route path="/collection" element={<FullCollection />} />
+//         <Route path="/add_game" element ={<AddGame />} />
+//         {/* Route with the ID parameter */}
+//         <Route path="/game_details/:id" element={<GameDetails />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
