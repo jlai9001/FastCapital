1d11cb55669d7d62ab79a2103ec59b27b41680e2
Jonathan Lai <jlai9001@gmail.com>
2025-05-21 14:30:52 -0700

    ver1

---
+++ client/src/pages/payment_modal.jsx
@@ -0,0 +1,17 @@
+import './payment_modal.css'
+
+function PaymentModal(){
+    // set states
+
+    // display elements
+    return (
+        <div className = "payment_modal">
+            <div className = "payment_bar">
+                <div className = "payment_title"> Payment Confirmation</div>
+            </div>
+
+        </div>
+    )
+}
+
+export default PaymentModal;
