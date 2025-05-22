aa4808bcab9b18552f653069ad338d2f6943e1db
Daniel <danielrgreenberg1@gmail.com>
2025-05-21 21:42:11 -0700

    004-Offer-Card_Backend get_businesses and get_offers endpoints created, code added for front/back end single repo

--- server/main.py
+++ server/main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic_schemas import OfferOut, BusinessOut
 from pathlib import Path
 import db
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost:5173",
+    "http://localhost:8080",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/offer/{offer_id}")
@@ -15,6 +33,10 @@ async def get_offer(offer_id: int) -> OfferOut:
         raise HTTPException(status_code=404, detail="Offer not found")
     return offer
 
+@app.get("/api/offer")
+async def get_offers() -> list[OfferOut]:
+    return db.get_offers()
+
 
 @app.get("/api/business/{business_id}")
 async def get_business(business_id: int) -> BusinessOut:
@@ -22,6 +44,10 @@ async def get_business(business_id: int) -> BusinessOut:
     if not business:
         raise HTTPException(status_code=404, detail="Business not found")
     return business
+
+@app.get("/api/business")
+async def get_businesses() -> list[BusinessOut]:
+    return db.get_businesses()
 
 
 @app.get("/{file_path}", response_class=FileResponse)
