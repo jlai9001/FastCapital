88f7b55b3f8326c6f7b64eb79f90ed51ccf00910
Jonathan Lai <jlai9001@gmail.com>
2025-06-06 12:04:16 -0700

    footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -15,25 +15,13 @@ const Footer = () => {
           </a>
         </div>
 
-        {/* Column 2: Links */}
-        <div className="footer__section">
-          <h2 className="footer__heading">Links</h2>
-          <ul className="footer__list">
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
-            <li><a href="/privacy">Privacy Policy</a></li>
-          </ul>
+        {/* Column 2: Logo */}
+        <div className="links-section">
+            Fast Capital
+
         </div>
 
-        {/* Column 3: Follow Us */}
-        <div className="footer__section">
-          <h2 className="footer__heading">Follow Us</h2>
-          <div className="footer__socials">
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
-            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          </div>
-        </div>
+
 
         {/* Column 4: Copyright */}
         <div className="footer__section footer__copyright-section">
