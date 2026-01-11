067c3a1c5eea3d25bd54d6973f11203c31ac8dc6
Jonathan Lai <jlai9001@gmail.com>
2026-01-11 12:25:42 -0800

    cosmetics placeholder

--- client/src/components/your-investments_chart.jsx
+++ client/src/components/your-investments_chart.jsx
@@ -128,7 +128,7 @@ const UserInvestments = () => {
 
       {!hasCompletedInvestments ? (
         <div className="empty-investments">
-          <p>You have no active investments that are fully funded.</p>
+          <p>You currently have no active investments that are fully funded.</p>
         </div>
       ) : (
         <div className="investments-dashboard">
