e02d212c19ef55d3dbce6675fb69f827e2b34c88
Jonathan Lai <jlai9001@gmail.com>
2026-01-25 17:03:41 -0800

    no message cleanup

--- client/src/pages/add-business.jsx
+++ client/src/pages/add-business.jsx
@@ -137,7 +137,7 @@ function AddBusiness() {
   // ----------------------------------
 const handleSubmit = async (e) => {
   e.preventDefault();
-  setMessage("Creating Business...");
+  setMessage("");
 
   // ✅ Don’t allow submit if file is invalid
   if (fileError) return;
